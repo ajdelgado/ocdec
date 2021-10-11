eca7ee2f35d709652c0587015c1be2f06788e835
Antonio J. Delgado <ad@susurrando.com>
2021-10-11 12:12:45 +0300

    Fix shellcheck
    Fix shellcheck

--- ocdec.sh
+++ ocdec.sh
@@ -53,18 +53,18 @@
 #
 # 1. Create directory structure for decrypted data
 #
-# export USER=testuser1
+# export oc_user=testuser1
 # export userLoginPass='s3cr3tpass'
 # cd /srv/ownclouddata
-# find ${USER}/files -type d -print0 |xargs -0 -I@ echo "@" |cut -sd / -f 2- |xargs -I@ mkdir -p "${USER}-DECRYPTED/@"
+# find ${oc_user}/files -type d -print0 |xargs -0 -I@ echo "@" |cut -sd / -f 2- |xargs -I@ mkdir -p "${oc_user}-DECRYPTED/@"
 #
 #
 # 2. Decrypt the data
 #
 # cd /srv/ownclouddata
-# find ${USER}/files -type f -print0 |xargs -0 -I@ sh -c '/tmp/ocdec.sh "@" > "${USER}-DECRYPTED/$(echo "@"|cut -sd / -f 2-)"'
+# find ${oc_user}/files -type f -print0 |xargs -0 -I@ sh -c '/tmp/ocdec.sh "@" > "${oc_user}-DECRYPTED/$(echo "@"|cut -sd / -f 2-)"'
 #
-# Now all your data is decrypted & available at the ${USER}-DECRYPTED/ path !
+# Now all your data is decrypted & available at the ${oc_user}-DECRYPTED/ path !
 #
 #
 
@@ -77,22 +77,22 @@
 # arg1: username
 # arg2: password
 function decryptUserPrivateKey() {
-  USER=$1
-  userLoginPass=$2
-  userPrivKeyPath="$USER/files_encryption/OC_DEFAULT_MODULE/$USER.privateKey"
-  if [ ! -r "$userPrivKeyPath" ]; then
-    echo "decryptUserPrivateKey():: File "$userPrivKeyPath" does not exist or not enough permissions! Aborting."
+  oc_user="${1}"
+  userLoginPass="${2}"
+  userPrivKeyPath="${oc_user}/files_encryption/OC_DEFAULT_MODULE/${oc_user}.privateKey"
+  if [ ! -r "${userPrivKeyPath}" ]; then
+    echo "decryptUserPrivateKey():: File '${userPrivKeyPath}' does not exist or not enough permissions! Aborting."
     return 1
   fi
 
-  encPrivKeyContentsALL="$(cat "$userPrivKeyPath")"
+  encPrivKeyContentsALL="$(cat "${userPrivKeyPath}")"
   # Some non-GNU versions of sed do not support pattern matches. In that case use: perl -pne 's/(00iv00.*)?00iv00.*/$1 /'
   encPrivKeyContentsBASE64="$( sed -r 's/^HBEGIN:.+:HEND-*//;s/(00iv00.*)?00iv00.*{16}xx/\1/' <(echo "${encPrivKeyContentsALL}") )"
   plainPrivKeyIV="$( sed -r 's/^HBEGIN.*00iv00//;s/xx$//' <(echo "${encPrivKeyContentsALL}") )"
-  userLoginPassHEX=$(echo -n $userLoginPass |od -An -tx1 |tr -dc '[:xdigit:]')
-  plainPrivKeyIVHEX=$(echo -n $plainPrivKeyIV |od -An -tx1 |tr -dc '[:xdigit:]')
+  userLoginPassHEX=$(echo -n "${userLoginPass}" |od -An -tx1 |tr -dc '[:xdigit:]')
+  plainPrivKeyIVHEX=$(echo -n "${plainPrivKeyIV}" |od -An -tx1 |tr -dc '[:xdigit:]')
 
-  echo "$( openssl enc -AES-256-CFB -d -nosalt -base64 -A -K $userLoginPassHEX -iv $plainPrivKeyIVHEX -in <(echo $encPrivKeyContentsBASE64) )"
+  openssl enc -AES-256-CFB -d -nosalt -base64 -A -K "${userLoginPassHEX}" -iv "${plainPrivKeyIVHEX}" -in <(echo "${encPrivKeyContentsBASE64}")
 }
 
 #
@@ -102,66 +102,66 @@ function decryptUserPrivateKey() {
 # arg2: path to an encrypted file (e.g. 'files/secretfile.txt' or 'files/Photos/Squirrel.jpg')
 # arg3: user Private Key in plaintext
 function decryptFile() {
-  USER="$1"
-  encFilePath="$2"
-  plainUserPrivKey="$3"
+  oc_user="${1}"
+  encFilePath="${2}"
+  plainUserPrivKey="${3}"
 
-  if [ ! -r "${USER}/$encFilePath" ]; then
-    echo "decryptFile():: File "${USER}/$encFilePath" does not exist or not enough permissions! Aborting."
+  if [ ! -r "${oc_user}/${encFilePath}" ]; then
+    echo "decryptFile():: File '${oc_user}/${encFilePath}' does not exist or not enough permissions! Aborting."
     return 1
   fi
-  if ! grep -q "^HBEGIN:" "${USER}/$encFilePath"; then
-    echo "decryptFile():: File "${USER}/$encFilePath" does not seem to be encrypted! Aborting."
+  if ! grep -q "^HBEGIN:" "${oc_user}/$encFilePath"; then
+    echo "decryptFile():: File '${oc_user}/${encFilePath}' does not seem to be encrypted! Aborting."
     return 1
   fi
 
   # --- Get the FileKey ---
-  userFileShareKeyPath="${USER}/files_encryption/keys/${encFilePath}/OC_DEFAULT_MODULE/${USER}.shareKey"
-  encFileKeyPath="${USER}/files_encryption/keys/${encFilePath}/OC_DEFAULT_MODULE/fileKey"
+  userFileShareKeyPath="${oc_user}/files_encryption/keys/${encFilePath}/OC_DEFAULT_MODULE/${oc_user}.shareKey"
+  encFileKeyPath="${oc_user}/files_encryption/keys/${encFilePath}/OC_DEFAULT_MODULE/fileKey"
 
-  decUserFileShareKeyHEX="$( openssl rsautl -decrypt -inkey <(echo "$plainUserPrivKey") -in "$userFileShareKeyPath" |od -An -tx1 |tr -dc '[:xdigit:]' )"
-  if [ -z "$decUserFileShareKeyHEX" ];then echo "decryptFile():: The User Private Key is not good. Are you sure your ownCloud User Login password is correct?"; return 1; fi
+  decUserFileShareKeyHEX="$( openssl rsautl -decrypt -inkey <(echo "${plainUserPrivKey}") -in "${userFileShareKeyPath}" |od -An -tx1 |tr -dc '[:xdigit:]' )"
+  if [ -z "${decUserFileShareKeyHEX}" ];then echo "decryptFile():: The User Private Key is not good. Are you sure your ownCloud User Login password is correct?"; return 1; fi
 
-  decFileKeyContent="$( openssl rc4 -d -in "$encFileKeyPath" -iv 0 -K $decUserFileShareKeyHEX )"
-  decFileKeyContentHEX="$( echo -n $decFileKeyContent |od -An -tx1 |tr -dc '[:xdigit:]' )"
-  decFileKeyContentHEX=${decFileKeyContentHEX:0:64}
+  decFileKeyContent="$( openssl rc4 -d -in "${encFileKeyPath}" -iv 0 -K "${decUserFileShareKeyHEX}" )"
+  decFileKeyContentHEX="$( echo -n "${decFileKeyContent}" |od -An -tx1 |tr -dc '[:xdigit:]' )"
+  decFileKeyContentHEX="${decFileKeyContentHEX:0:64}"
   # --- Get the FileKey ---
 
   # --- Decrypt the file ---
   # OC writes the encrypted file in 8K chunks, each containing it's own iv in the end
   chunkSize=8192
-  while read -d '' -n $chunkSize CHUNK || [ ! -z "$CHUNK" ]; do
+  while read -r -d '' -n $chunkSize CHUNK || [ -n "$CHUNK" ]; do
     #split chunk into payload an iv string (strip padding from iv)          
-    read payload iv <<<`echo $CHUNK | sed -r 's/(.*)00iv00(.{16})xx/\1 \2/'`
+    read -r payload iv <<<"$(echo "${CHUNK}" | sed -r 's/(.*)00iv00(.{16})xx/\1 \2/')"
     CHUNK=
     #if [ -z $CHUNK ]; then break; fi
     # convert base64 iv into hex
     iv=$(echo -n "$iv" | od -An -tx1 | tr -dc '[:xdigit:]' )
     # decode chunk
-    openssl enc -AES-256-CFB -d -nosalt -base64 -A -K $decFileKeyContentHEX -iv $iv -in <(echo "$payload")
-    #done <<<`sed -r 's/^HBEGIN:.+:HEND-*//' <"${USER}/$encFilePath"` # pipe the encrypted file without head into the loop
-    done < <(dd bs=$chunkSize skip=1 if="${USER}/$encFilePath")
+    openssl enc -AES-256-CFB -d -nosalt -base64 -A -K "${decFileKeyContentHEX}" -iv "${iv}" -in <(echo "${payload}")
+    #done <<<`sed -r 's/^HBEGIN:.+:HEND-*//' <"${oc_user}/${encFilePath}"` # pipe the encrypted file without head into the loop
+    done < <(dd bs="${chunkSize}" skip=1 if="${oc_user}/${encFilePath}")
   # --- Decrypt the file ---
 }
 
 # Get a username from the path (arg1)
-USER="$(echo "$1" |cut -sd / -f 1)"
+oc_user="$(echo "${1}" |cut -sd / -f 1)"
 
 # Strip off the username from the path (arg1)
-FILETD="$(echo "$1" |cut -sd / -f 2-)"
+FILETD="$(echo "${1}" |cut -sd / -f 2-)"
 
-if [ ! -r "$USER" ]; then
+if [ ! -r "${oc_user}" ]; then
   echo "User directory cannot be found! Are you sure you are in ownCloud's data directory?"
   exit 1
 fi
 
-if [ -z "$userLoginPass" ]; then
+if [ -z "${userLoginPass}" ]; then
   echo "Please set userLoginPass environment variable!"
   exit 1
 fi
 
-if [ ! -r "$1" ]; then
-  echo "File specified $1 does not exist or not enough permissions to access it!"
+if [ ! -r "${1}" ]; then
+  echo "File specified ${1} does not exist or not enough permissions to access it!"
   exit 1
 fi
 
@@ -173,11 +173,11 @@ fi
 # 1) Locate and decrypt User Private Key
 #
 # TODO: to decrypt User Private Key only once when running this script in a loop (decrypting multiple files)
-plainUserPrivKey="$(decryptUserPrivateKey $USER $userLoginPass)";
+plainUserPrivKey="$(decryptUserPrivateKey "${oc_user}" "${userLoginPass}")";
 
 #
 # 2) Decrypt the shareKey, then the fileKey, then the file and output the plaintext
 #
-decryptFile $USER "$FILETD" "$plainUserPrivKey"
+decryptFile "${oc_user}" "${FILETD}" "${plainUserPrivKey}"
 
 # End of a script
